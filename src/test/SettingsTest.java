7603ecf02983ddad7ed83c190f3f6297265ae6bd
iceiceice <duanjinyu0403@163.com>
2023-12-01 16:05:42 +0800

    [CHG] 规范代码格式

--- src/test/SettingsTest.java
+++ src/test/SettingsTest.java
@@ -4,216 +4,213 @@
  */
 package test;
 
+import core.Settings;
 import java.io.File;
 import java.io.PrintWriter;
-
 import junit.framework.TestCase;
-import core.Settings;
 
-/**
- * Tests Settings class' different setting getting methods
- */
+/** Tests Settings class' different setting getting methods */
 public class SettingsTest extends TestCase {
-	private static final String IRS_S = "invalidRunSetting";
+  private static final String IRS_S = "invalidRunSetting";
 
-	private static final String CSV_RS_S = "csvRunSetting";
-	private static final int[] CSV_RS_V = {1,2,3,4};
+  private static final String CSV_RS_S = "csvRunSetting";
+  private static final int[] CSV_RS_V = {1, 2, 3, 4};
 
-	private static final String TST = "tstSetting";
-	private static final String TST_RES = "tst";
-	private static final String[] INPUT = {
-		"Ns.setting1 = 1",
-		"Ns.setting2 = true",
-		TST + " = " + TST_RES,
-		"tstSetting2 = tst2",
-		"double = 1.1",
-		"csvDoubles = 1.1,2.2,3.3",
-		"csvInts 1,2,3",
-		"booleanTrue = true",
-		"booleanFalse = false",
-		"int = 1",
-		"runSetting = [val1 ; val2;val3; val4 ]",
-		IRS_S + " = [val1 ; val2",
-		CSV_RS_S + " = [" + CSV_RS_V[0]+","+CSV_RS_V[1]+";"+CSV_RS_V[2]+","+CSV_RS_V[3]+"]",
-		"Ns.runSetting = [ ; ; 2; ]",
-		"DefNs.runSetting = 1"
+  private static final String TST = "tstSetting";
+  private static final String TST_RES = "tst";
+  private static final String[] INPUT = {
+    "Ns.setting1 = 1",
+    "Ns.setting2 = true",
+    TST + " = " + TST_RES,
+    "tstSetting2 = tst2",
+    "double = 1.1",
+    "csvDoubles = 1.1,2.2,3.3",
+    "csvInts 1,2,3",
+    "booleanTrue = true",
+    "booleanFalse = false",
+    "int = 1",
+    "runSetting = [val1 ; val2;val3; val4 ]",
+    IRS_S + " = [val1 ; val2",
+    CSV_RS_S
+        + " = ["
+        + CSV_RS_V[0]
+        + ","
+        + CSV_RS_V[1]
+        + ";"
+        + CSV_RS_V[2]
+        + ","
+        + CSV_RS_V[3]
+        + "]",
+    "Ns.runSetting = [ ; ; 2; ]",
+    "DefNs.runSetting = 1"
+  };
 
-	};
+  private String RS_S = "runSetting";
 
-	private String RS_S = "runSetting";
+  private Settings s;
 
+  protected void setUp() throws Exception {
+    super.setUp();
+    File tempFile = File.createTempFile("settingsTest", ".tmp");
+    tempFile.deleteOnExit();
 
-	private Settings s;
+    PrintWriter out = new PrintWriter(tempFile);
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		File tempFile = File.createTempFile("settingsTest", ".tmp");
-		tempFile.deleteOnExit();
+    for (String s : INPUT) {
+      out.println(s);
+    }
+    out.close();
 
-		PrintWriter out = new PrintWriter(tempFile);
+    Settings.init(tempFile.getAbsolutePath());
+    s = new Settings();
+  }
 
-		for (String s : INPUT) {
-			out.println(s);
-		}
-		out.close();
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    Settings.setRunIndex(0);
+  }
 
-		Settings.init(tempFile.getAbsolutePath());
-		s = new Settings();
-	}
+  public void testContains() {
+    assertTrue(s.contains("Ns.setting1"));
+    assertTrue(s.contains("Ns.setting2"));
+  }
 
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		Settings.setRunIndex(0);
-	}
+  public void testGetSetting() {
+    assertEquals("1", s.getSetting("Ns.setting1"));
+    assertEquals("true", s.getSetting("Ns.setting2"));
+  }
 
+  public void testGetDouble() {
+    assertEquals(1.1, s.getDouble("double"));
+  }
 
-	public void testContains() {
-		assertTrue(s.contains("Ns.setting1"));
-		assertTrue(s.contains("Ns.setting2"));
-	}
+  public void testGetCsvSetting() {
+    String[] csv = s.getCsvSetting("csvInts", 3);
+    assertEquals(csv.length, 3);
+    assertEquals("1", csv[0]);
+    assertEquals("2", csv[1]);
+    assertEquals("3", csv[2]);
+  }
 
-	public void testGetSetting() {
-		assertEquals("1", s.getSetting("Ns.setting1"));
-		assertEquals("true", s.getSetting("Ns.setting2"));
-	}
+  public void testGetCsvDoubles() {
+    double[] csv = s.getCsvDoubles("csvDoubles", 3);
+    assertEquals(csv.length, 3);
+    assertEquals(1.1, csv[0]);
+    assertEquals(2.2, csv[1]);
+    assertEquals(3.3, csv[2]);
+  }
 
-	public void testGetDouble() {
-		assertEquals(1.1, s.getDouble("double"));
-	}
+  public void testGetCsvDoublesUnknownAmount() {
+    double[] csv = s.getCsvDoubles("csvDoubles");
+    assertEquals(csv.length, 3);
+    assertEquals(1.1, csv[0]);
+    assertEquals(2.2, csv[1]);
+    assertEquals(3.3, csv[2]);
+  }
 
-	public void testGetCsvSetting() {
-		String[] csv = s.getCsvSetting("csvInts",3);
-		assertEquals(csv.length, 3);
-		assertEquals("1",csv[0]);
-		assertEquals("2",csv[1]);
-		assertEquals("3",csv[2]);
-	}
+  public void testGetCsvInts() {
+    int[] csv = s.getCsvInts("csvInts", 3);
+    assertEquals(csv.length, 3);
+    assertEquals(1, csv[0]);
+    assertEquals(2, csv[1]);
+    assertEquals(3, csv[2]);
+  }
 
-	public void testGetCsvDoubles() {
-		double[] csv = s.getCsvDoubles("csvDoubles",3);
-		assertEquals(csv.length, 3);
-		assertEquals(1.1,csv[0]);
-		assertEquals(2.2,csv[1]);
-		assertEquals(3.3,csv[2]);
-	}
+  public void testGetCsvIntsUnknownAmount() {
+    int[] csv = s.getCsvInts("csvInts");
+    assertEquals(csv.length, 3);
+    assertEquals(1, csv[0]);
+    assertEquals(2, csv[1]);
+    assertEquals(3, csv[2]);
+  }
 
-	public void testGetCsvDoublesUnknownAmount() {
-		double[] csv = s.getCsvDoubles("csvDoubles");
-		assertEquals(csv.length, 3);
-		assertEquals(1.1,csv[0]);
-		assertEquals(2.2,csv[1]);
-		assertEquals(3.3,csv[2]);
-	}
+  public void testGetInt() {
+    assertEquals(1, s.getInt("int"));
+  }
 
-	public void testGetCsvInts() {
-		int[] csv = s.getCsvInts("csvInts",3);
-		assertEquals(csv.length, 3);
-		assertEquals(1,csv[0]);
-		assertEquals(2,csv[1]);
-		assertEquals(3,csv[2]);
-	}
+  public void testGetBoolean() {
+    assertTrue(s.getBoolean("booleanTrue"));
+    assertFalse(s.getBoolean("booleanFalse"));
+  }
 
-	public void testGetCsvIntsUnknownAmount() {
-		int[] csv = s.getCsvInts("csvInts");
-		assertEquals(csv.length, 3);
-		assertEquals(1,csv[0]);
-		assertEquals(2,csv[1]);
-		assertEquals(3,csv[2]);
-	}
+  public void testCreateIntializedObject() {
+    Object o = s.createIntializedObject("movement.RandomWaypoint");
+    assertTrue(o instanceof movement.RandomWaypoint);
+  }
 
-	public void testGetInt() {
-		assertEquals(1,s.getInt("int"));
-	}
+  public void testValueFillString() {
+    String test = "1-%%tstSetting%%-2-%%tstSetting2%%";
+    String result = s.valueFillString(test);
+    assertEquals("1-tst-2-tst2", result);
 
-	public void testGetBoolean() {
-		assertTrue(s.getBoolean("booleanTrue"));
-		assertFalse(s.getBoolean("booleanFalse"));
-	}
+    result = s.valueFillString("%%" + TST + "%%-aaa");
+    assertEquals(TST_RES + "-aaa", result);
 
-	public void testCreateIntializedObject() {
-		Object o = s.createIntializedObject("movement.RandomWaypoint");
-		assertTrue(o instanceof movement.RandomWaypoint);
-	}
+    result = s.valueFillString("%%" + TST + "%%");
+    assertEquals(TST_RES, result);
+  }
 
+  public void testRunIndex() {
+    assertEquals(s.getSetting(RS_S), "val1");
+    Settings.setRunIndex(1);
+    assertEquals(s.getSetting(RS_S), "val2");
+    Settings.setRunIndex(2);
+    assertEquals(s.getSetting(RS_S), "val3");
+    Settings.setRunIndex(3);
+    assertEquals(s.getSetting(RS_S), "val4");
+    Settings.setRunIndex(4);
+    assertEquals(s.getSetting(RS_S), "val1"); // should wrap around
+    Settings.setRunIndex(5);
+    assertEquals(s.getSetting(RS_S), "val2");
+  }
 
-	public void testValueFillString() {
-		String test = "1-%%tstSetting%%-2-%%tstSetting2%%";
-		String result = s.valueFillString(test);
-		assertEquals("1-tst-2-tst2",result);
+  public void testRunIndexContains() {
+    assertFalse(s.contains("Ns.runSetting"));
+    Settings.setRunIndex(2);
+    assertTrue(s.contains("Ns.runSetting"));
+  }
 
-		result = s.valueFillString("%%"+TST+"%%-aaa");
-		assertEquals(TST_RES + "-aaa",result);
+  /** Test filling empty values of run index from secondary namespace */
+  public void testEmptyRunIndex() {
+    String rs = "runSetting";
+    Settings s = new Settings("Ns");
+    s.setSecondaryNamespace("DefNs");
+    assertEquals(s.getInt(rs), 1);
+    Settings.setRunIndex(1);
+    assertEquals(s.getInt(rs), 1);
 
-		result = s.valueFillString("%%"+TST+"%%");
-		assertEquals(TST_RES,result);
-	}
+    Settings.setRunIndex(2);
+    assertEquals(s.getInt(rs), 2); // the only defined value
 
-	public void testRunIndex() {
-		assertEquals(s.getSetting(RS_S), "val1");
-		Settings.setRunIndex(1);
-		assertEquals(s.getSetting(RS_S), "val2");
-		Settings.setRunIndex(2);
-		assertEquals(s.getSetting(RS_S), "val3");
-		Settings.setRunIndex(3);
-		assertEquals(s.getSetting(RS_S), "val4");
-		Settings.setRunIndex(4);
-		assertEquals(s.getSetting(RS_S), "val1"); // should wrap around
-		Settings.setRunIndex(5);
-		assertEquals(s.getSetting(RS_S), "val2");
-	}
+    Settings.setRunIndex(3);
+    assertEquals(s.getInt(rs), 1);
+  }
 
-	public void testRunIndexContains() {
-		assertFalse(s.contains("Ns.runSetting"));
-		Settings.setRunIndex(2);
-		assertTrue(s.contains("Ns.runSetting"));
-	}
+  public void testRunIndexCSVs() {
+    // test CSVs
+    int[] vals = s.getCsvInts(CSV_RS_S, 2);
+    assertEquals(CSV_RS_V[0], vals[0]);
+    assertEquals(CSV_RS_V[1], vals[1]);
 
-	/**
-	 * Test filling empty values of run index from secondary namespace
-	 */
-	public void testEmptyRunIndex() {
-		String rs = "runSetting";
-		Settings s = new Settings("Ns");
-		s.setSecondaryNamespace("DefNs");
-		assertEquals(s.getInt(rs), 1);
-		Settings.setRunIndex(1);
-		assertEquals(s.getInt(rs), 1);
+    Settings.setRunIndex(1);
+    vals = s.getCsvInts(CSV_RS_S, 2);
+    assertEquals(CSV_RS_V[2], vals[0]);
+    assertEquals(CSV_RS_V[3], vals[1]);
 
-		Settings.setRunIndex(2);
-		assertEquals(s.getInt(rs), 2); // the only defined value
+    Settings.setRunIndex(2); // wrap around
+    vals = s.getCsvInts(CSV_RS_S, 2);
+    assertEquals(CSV_RS_V[0], vals[0]);
+    assertEquals(CSV_RS_V[1], vals[1]);
+  }
 
-		Settings.setRunIndex(3);
-		assertEquals(s.getInt(rs), 1);
-	}
+  public void testInvalidRunIndex() {
+    assertEquals("[val1 ; val2", s.getSetting(IRS_S));
+  }
 
-	public void testRunIndexCSVs() {
-		// test CSVs
-		int [] vals = s.getCsvInts(CSV_RS_S, 2);
-		assertEquals(CSV_RS_V[0],vals[0]);
-		assertEquals(CSV_RS_V[1],vals[1]);
-
-		Settings.setRunIndex(1);
-		vals = s.getCsvInts(CSV_RS_S, 2);
-		assertEquals(CSV_RS_V[2],vals[0]);
-		assertEquals(CSV_RS_V[3],vals[1]);
-
-		Settings.setRunIndex(2); // wrap around
-		vals = s.getCsvInts(CSV_RS_S, 2);
-		assertEquals(CSV_RS_V[0],vals[0]);
-		assertEquals(CSV_RS_V[1],vals[1]);
-	}
-
-	public void testInvalidRunIndex() {
-		assertEquals("[val1 ; val2",s.getSetting(IRS_S));
-	}
-
-	/**
-	 * Tests disabled run-specific variables
-	 */
-	public void testNoRun() {
-		Settings.setRunIndex(-1);
-		assertEquals("[val1 ; val2;val3; val4 ]", s.getSetting(RS_S));
-	}
-
+  /** Tests disabled run-specific variables */
+  public void testNoRun() {
+    Settings.setRunIndex(-1);
+    assertEquals("[val1 ; val2;val3; val4 ]", s.getSetting(RS_S));
+  }
 }
